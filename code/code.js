c834e5a675e75b96929311317b6522def9c50e51
DilanAlpe <eduardodilanalpe@gmail.com>
2021-11-30 15:49:57 -0600

    entrega a nido

--- code/code.js
+++ code/code.js
@@ -3,58 +3,34 @@
 (function(){
   //variables
   
-<<<<<<< HEAD
   var btncal = document.getElementById('calcular'),
-=======
-  import jsPDF from 'jspdf';
-  var name = document.getElementById('ingrediente'),
-  btncal = document.getElementById('calcular'),
->>>>>>> d4ddde66875a8d5eadff708aca1a2d79ea6c2c0a
   btnadd = document.getElementById('add'),
   btnrem = document.getElementById('rest'),
   box = document.getElementById("box");
 
   //funciones
   var calcular = function(){
-    var elem_unidades = document.getElementsByClassName('unidades')
-    var elem_precio = document.getElementsByClassName('precio')
-    var elem_resultado = document.getElementsByClassName('resultado')
-<<<<<<< HEAD
+    var elem_unidades = document.getElementsByClassName('unidades'),
+    elem_precio = document.getElementsByClassName('precio'),
+    elem_resultado = document.getElementsByClassName('resultado');
+    let arr = [],
+     total = 0;
 
     for (i=0;i<elem_precio.length;i++){
-      const arr = [];
-      let total = 0;
+     
       div = elem_unidades[i].value /elem_precio[i].value;
       por = div * .5;
       suma = div + por; 
       elem_resultado[i].innerHTML = ("$"+ suma.toFixed(2));  
       arr.push(suma);
       console.log(arr);
-
-
     }
-=======
-
-    for (i=0;i<elem_precio.length;i++){
-      elem_resultado[i].innerHTML = ("$"+ elem_unidades[i].value *elem_precio[i].value);
+    for(i of arr){
+      total+=i;
+      document.getElementById('total').innerHTML = ("$"+ total.toFixed(2));
     }
 
-    console.log(elem_precio)
-    var doc = new jsPDF();
-    doc.text(20, 20, 'Hola mundo!');
-    doc.text(20, 30, 'Esto es un generador de PDF en Java Script.');  
-    doc.addPage();
-    doc.text(20, 20, '¿Cómo estas?');
-
-    //var u = Number(document.getElementById('unidades').value),
-    //pr = Number(document.getElementById('precio').value);
-
-   // var opr = (u/pr);
-   // var por = opr *.5;
-   // var suma = opr + por;
-
-   //document.getElementById('resultado').innerHTML = ("$"+suma);
->>>>>>> d4ddde66875a8d5eadff708aca1a2d79ea6c2c0a
+    console.log(total);
   };
 
 
@@ -115,26 +91,3 @@
   btnrem.addEventListener("click", remdiv )
 
 }());
-
-function getPDF() {
-  var doc = new jsPDF();
- 
-  // We'll make our own renderer to skip this editor
-  var specialElementHandlers = {
-    '#getPDF': function(element, renderer){
-      return true;
-    },
-    '.controls': function(element, renderer){
-      return true;
-    }
-  };
-
-  // All units are in the set measurement for the document
-  // This can be changed to "pt" (points), "mm" (Default), "cm", "in"
-  doc.fromHTML($('.zima').get(0), 15, 15, {
-    'width': 170, 
-    'elementHandlers': specialElementHandlers
-  });
-
-  doc.save('Generated.pdf');
-}
--- code/pdf.js
+++ code/pdf.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
   document.addEventListener("DOMContentLoaded", () => {
     // Escuchamos el click del botón
     var date = Date();
@@ -33,27 +33,3 @@
             .catch(err => console.log(err));
     });
   });
-=======
-function getPDF() {
-    var doc = new jsPDF();
-   
-    // We'll make our own renderer to skip this editor
-    var specialElementHandlers = {
-      '#getPDF': function(element, renderer){
-        return true;
-      },
-      '.controls': function(element, renderer){
-        return true;
-      }
-    };
-  
-    // All units are in the set measurement for the document
-    // This can be changed to "pt" (points), "mm" (Default), "cm", "in"
-    doc.fromHTML($('.zima').get(0), 15, 15, {
-      'width': 170, 
-      'elementHandlers': specialElementHandlers
-    });
-  
-    doc.save('Generated.pdf');
-  }
->>>>>>> d4ddde66875a8d5eadff708aca1a2d79ea6c2c0a
